971b01e36278a4d577e453890de3cf10f3a1d967
DanielMarella <marella.daniel96@gmail.com>
2023-08-22 17:28:49 +0200

    create a index page for visualize my posts

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-12">
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                      <th scope="col">Id</th>
+                      <th scope="col">Title</th>
+                      <th scope="col">Slug</th>
+                      <th scope="col">Actions</th>
+                    </tr>
+                  </thead>
+                
+                <tbody>
+                    @foreach ($posts as $post)
+                        <tr>
+                            <th>{{$post -> id}}</th>
+                            <td>{{$post -> title}}</td>
+                            <td>{{$post -> slug}}</td>
+                            <td>
+                                <a href="" class="btn btn-sm btn-success">
+                                    Edit
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+                
+            </table>
+        </div>
+    </div>
+</div>
+@endsection
