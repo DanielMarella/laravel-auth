f9b7dfefa4c223e5245864ab3fb03b045b847145
DanielMarella <marella.daniel96@gmail.com>
2023-08-22 16:30:56 +0200

    create a new controller for guest, create homepage for guest, adding route for guest controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
+use App\Http\Controllers\Guest\HomeController as GuestHomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,15 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
 
 
 Route::prefix('admin')->name('admin.')->middleware('auth')->group(function () {
-    Route::get('/', [ DashboardController::class, 'home'])->name('home');
+    Route::get('/', [ AdminDashboardController::class, 'home'])->name('home');
+ 
+});
+
+Route::name('guest.')->group(function () {
+    Route::get('/', [ GuestHomeController::class, 'home'])->name('home');
  
 });
